8840146332633a6ecffa09ba86b121e1e96407a7
Allen_Work <allenwangook@gmail.com>
2018-11-23 17:03:47 +0800

    interview plan

--- src/main/java/allen/interview/thread/chapter04/HashMapDeadLock.java
+++ src/main/java/allen/interview/thread/chapter04/HashMapDeadLock.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
  * @date 2018年11月06日 8:37
  */
 public class HashMapDeadLock {
-	private HashMap<String, String> map = new HashMap<String, String>();
+	private HashMap<String, String> map = new HashMap<>();
 
 	private void add(String key, String value) {
 		this.map.put(key, value);
@@ -20,7 +20,7 @@ public class HashMapDeadLock {
 	public static void main(String[] args) {
 		//慎重启动该方法，因为会让你的机器死机  兄弟
 		HashMapDeadLock hashMapDeadLock = new HashMapDeadLock();
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 2; i++) {//创建两个线程，启动
 			new Thread(() -> {
 				for (int j = 0; j < Integer.MAX_VALUE; j++) {
 					hashMapDeadLock.add(String.valueOf(j), String.valueOf(j));
--- src/main/java/allen/interview/thread/chapter08/ThreadPoolTest.java
+++ src/main/java/allen/interview/thread/chapter08/ThreadPoolTest.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class ThreadPoolTest {
 	public static void main(String[] args) throws InterruptedException {
 		final ThreadPool threadPool = new BasicThreadPool(2, 6, 4, 1000);
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 20; i++) {//
 			threadPool.execute(()->{
 				try {
 					TimeUnit.SECONDS.sleep(10);
