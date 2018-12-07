6a1e98995e12d0adc09bab677b09add823dd5e60
Allen_Work <allenwangook@gmail.com>
2018-12-07 11:10:51 +0800

    finish Bubble Sort

---
+++ src/main/java/allen/interview/JavaAlgo/Bubble.java
@@ -0,0 +1,26 @@
+package allen.interview.JavaAlgo;
+
+/**
+ * 冒泡排序
+ * 一轮循环后，未排序中最大的转移到最右端。 没有移动则说明是有序的
+ * @author WangJianQiang
+ * @Description:
+ * @date 2018年12月07日 10:05
+ */
+public class Bubble<T extends Comparable<T>> extends Sort<T> {
+	@Override
+	public void sort(T[] nums) {
+		int N = nums.length;
+		//用来记录对一次未排序遍历后是否有移动
+		boolean hasSorted = false;
+		for (int i = N - 1; i > 0 && !hasSorted; i--) {
+			hasSorted = true;
+			for (int j = 0; j < i; j++) {
+				if (less(nums[j + 1], nums[j])) {
+					hasSorted = false;
+					swap(nums, j, j + 1);
+				}
+			}
+		}
+	}
+}
--- src/main/java/allen/interview/Teat.java
+++ src/main/java/allen/interview/Teat.java
@@ -1,5 +1,6 @@
 package allen.interview;
 
+import allen.interview.JavaAlgo.Bubble;
 import allen.interview.JavaAlgo.Selection;
 
 import java.util.Arrays;
@@ -9,8 +10,13 @@ public class Teat {
 
 	public static void main(String[] args) {
 		Integer[] a = {49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 1};
-		Selection<Integer> selection=new Selection<>();
-		selection.sort(a);
+		/*选择排序测试*/
+//		Selection<Integer> selection=new Selection<>();
+//		selection.sort(a);
+		/*冒泡排序测试*/
+		Bubble<Integer> bubble=new Bubble<>();
+		bubble.sort(a);
+
 		System.out.println(Arrays.asList(a));
 	}
 
