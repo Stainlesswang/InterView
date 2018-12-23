d81307d7a6c90cb4d8b19670381b125696b42d8a
allen <allenwangook@gmail.com>
2018-12-23 22:43:41 +0800

    FreeMarker demo

--- src/main/java/allen/interview/JavaAlgo/QuickSort.java
+++ src/main/java/allen/interview/JavaAlgo/QuickSort.java
@@ -8,41 +8,50 @@ import java.util.List;
  * QuickSort 快速排序，寻找一个基数进行分组，
  * 左边都是小于基数的，右边都是大于基数的
  * 然后分别对两个分组里的数据进行排序，排完后数组即有序
+ * 算法的关键在于切分的算法，
+ * 1.对于某个j，a[j]已经排定;
+ * 2.a[lo]到a[j-1]中的所有元素都不大于a[j]
+ * 3.a[j+1]到a[hi]中的所有元素都不小于a[j]
+ *
+ *
+ * QuickSort优化：
+ * 1.在数组较小（1到15）这种的规模下，插入排序要优于快速排序，可以利用这一点在递归的时候使用InsertSort
+ * 2.三项切分，该方法适用于数据中含有较多重复元素的情况下，分组的是否多加一组相等元素
  * @author WangJianQiang
  * @Description:
  * @date 2018年12月13日 9:35
  */
 public class QuickSort<T extends Comparable<T>> extends Sort<T> {
-	@Override
-	public void sort(T[] nums) {
-		shuffle(nums);
-		sort(nums, 0, nums.length - 1);
-	}
+    @Override
+    public void sort(T[] nums) {
+        shuffle(nums);
+        sort(nums, 0, nums.length - 1);
+    }
 
-	private void sort(T[] nums, int l, int h) {
-		if (h <= l)
-			return;
-		int j = partition(nums, l, h);
-		sort(nums, l, j - 1);
-		sort(nums, j + 1, h);
-	}
+    private void sort(T[] nums, int l, int h) {
+        if (h <= l)
+            return;
+        int j = partition(nums, l, h);
+        sort(nums, l, j - 1);
+        sort(nums, j + 1, h);
+    }
 
-	private void shuffle(T[] nums) {
-		List<Comparable> list = Arrays.asList(nums);
-		Collections.shuffle(list);
-		list.toArray(nums);
-	}
-	private int partition(T[] nums, int l, int h) {
-		int i = l, j = h + 1;
-		T v = nums[l];
-		while (true) {
-			while (less(nums[++i], v) && i != h) ;
-			while (less(v, nums[--j]) && j != l) ;
-			if (i >= j)
-				break;
-			swap(nums, i, j);
-		}
-		swap(nums, l, j);
-		return j;
-	}
+    private void shuffle(T[] nums) {
+        List<Comparable> list = Arrays.asList(nums);
+        Collections.shuffle(list);
+        list.toArray(nums);
+    }
+
+    private int partition(T[] nums, int l, int h) {
+        int i = l, j = h + 1;
+        T v = nums[l];
+        while (true) {
+            while (less(nums[++i], v)) if (i==h) break;
+            while (less(v, nums[--j])) if (j==l) break;
+            if (i >= j) break;
+            swap(nums, i, j);
+        }
+        swap(nums, l, j);
+        return j;
+    }
 }
