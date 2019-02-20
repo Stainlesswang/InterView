d2fb8c4c1ea7e93feb8577759ac2cfb4c522d263
Allen_Work <allenwangook@gmail.com>
2019-02-20 16:44:20 +0800

    learn HashMap source code

---
+++ src/main/java/allen/interview/collection/HashMapLearn.java
@@ -0,0 +1,23 @@
+package allen.interview.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author WangJianQiang
+ * @Description: HashMap特性学习相关记录
+ * @date 2019年02月20日 10:18
+ */
+public class HashMapLearn {
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		int a=1;
+		if (1==a)
+			System.out.println("aaaa");
+		do {
+			if (a>0)
+				System.out.println("都");
+		} while (a != 1);
+	}
+}
