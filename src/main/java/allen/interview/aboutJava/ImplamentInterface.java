b674cc32d38ead8b90c012c65ad7bea903319df1
Allen_Work <allenwangook@gmail.com>
2019-04-08 18:50:12 +0800

    change package position

--- src/main/java/allen/interview/aboutJava/ImplamentInterface.java
+++ src/main/java/allen/interview/aboutJava/ImplamentInterface.java
@@ -5,10 +5,9 @@ package allen.interview.aboutJava;
  * @Description:
  * @date 2019年03月14日 17:17
  */
-public class ImplamentInterface implements InterfaceTest,InterfaceTest2{
+public class ImplamentInterface implements InterfaceTest, InterfaceTest2 {
 	@Override
 	public void test() {
-
 	}
 
 	@Override
--- src/main/java/allen/interview/aboutJava/InterfaceTest.java
+++ src/main/java/allen/interview/aboutJava/InterfaceTest.java
@@ -9,13 +9,16 @@ public interface InterfaceTest {
 	//变量默认是  static final的
 	int a = 9;
 	int b = 9;
+
 	//只能由接口名称来调用static方法
-	static void tre(){
+	static void tre() {
 
 	}
+
 	//必须是实现类来使用default方法，并且当多继承的时候继承了两个含有相同default的话
 	default void test() {
 		System.out.println("interface default method");
 	}
-	 void interMethod();
+
+	void interMethod();
 }
