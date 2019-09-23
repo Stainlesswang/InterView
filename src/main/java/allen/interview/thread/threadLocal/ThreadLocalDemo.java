eb98776659464a1e50c41014ea65ffac3a267743
allen <allenwangook@gmail.com>
2019-09-23 19:14:23 +0800

    learn threadLocal

--- src/main/java/allen/interview/thread/threadLocal/ThreadLocalDemo.java
+++ src/main/java/allen/interview/thread/threadLocal/ThreadLocalDemo.java
@@ -10,14 +10,30 @@ package allen.interview.thread.threadLocal;
  *    当前访问线程id作为key,用户对象信息作为value存储下来
  */
 public class ThreadLocalDemo {
+    /**
+     * 定义一个threadLocal变量
+     */
     private final  static ThreadLocal<String> threadLocal=new ThreadLocal<>();
 
+    /**
+     * 当前线程为key, threadLocal作为对象塞进去
+     * @param obj
+     */
     public static void addUser(String obj){
         threadLocal.set(obj);
     }
+
+    /**
+     * 根据当前线程取出threadLocal变量, 然后再取出其中保存的数据
+     * @return
+     */
     public static String getUser(){
         return  threadLocal.get();
     }
+
+    /**
+     * 把当前的threadLocal移除
+     */
     public static void remove(){
         threadLocal.remove();
     }
