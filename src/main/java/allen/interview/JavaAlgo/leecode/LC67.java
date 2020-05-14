3efc0847371e3b0d5fc67cf39762703655dddf4a
allen <allenwangook@gmail.com>
2020-05-14 21:03:18 +0800

    Spring NamedJdbcTemplate

---
+++ src/main/java/allen/interview/JavaAlgo/leecode/LC67.java
@@ -0,0 +1,8 @@
+package allen.interview.JavaAlgo.leecode;
+
+/**
+ * @author AllenWong
+ * @date 2020/5/14 11:36 AM
+ */
+public class LC67 {
+}
---
+++ src/main/java/allen/interview/Main.java
@@ -0,0 +1,8 @@
+package allen.interview;
+
+/**
+ * @author AllenWong
+ * @date 2020/5/14 5:22 PM
+ */
+public class Main {
+}
