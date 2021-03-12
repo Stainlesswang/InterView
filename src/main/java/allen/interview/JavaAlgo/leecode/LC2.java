c0bbb4e4a681517dbb7ee6e0c927e60a2b2a2acb
allen <allenwangook@gmail.com>
2021-03-12 11:16:02 +0800

    learn leecode 2

--- src/main/java/allen/interview/JavaAlgo/leecode/LC2.java
+++ src/main/java/allen/interview/JavaAlgo/leecode/LC2.java
@@ -5,20 +5,23 @@ package allen.interview.JavaAlgo.leecode;
  * @date 2020/4/21 8:30 PM
  */
 public class LC2 {
-    public class ListNode {
-     int val;
-      ListNode next;
-      ListNode(int x) { val = x; }
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
     }
 
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode resultHead= new ListNode(0);
-        ListNode p=l1,q=l2,curr=resultHead;
-        int more=0;
-        while(null!=p || null !=q){
-            int x = (null==p)? 0:p.val;
-            int y = (null==q)? 0:q.val;
+        ListNode resultHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = resultHead;
+        int more = 0;
+        while (null != p || null != q) {
+            int x = (null == p) ? 0 : p.val;
+            int y = (null == q) ? 0 : q.val;
             int sum = x + y + more;
             more = sum / 10;
             curr.next = new ListNode(sum % 10);
@@ -26,8 +29,8 @@ public class LC2 {
             if (p != null) p = p.next;
             if (q != null) q = q.next;
         }
-        if (more >0){
-            curr.next= new ListNode(more);
+        if (more > 0) {
+            curr.next = new ListNode(more);
         }
         return resultHead.next;
     }
