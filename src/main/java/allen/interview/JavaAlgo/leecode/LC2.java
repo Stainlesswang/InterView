2f8cf66c9cd893b75b919bc4518a611685f92ecb
allen <allenwangook@gmail.com>
2021-08-07 14:54:22 +0800

    fix leecode

--- src/main/java/allen/interview/JavaAlgo/leecode/LC2.java
+++ src/main/java/allen/interview/JavaAlgo/leecode/LC2.java
@@ -16,22 +16,22 @@ public class LC2 {
 
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode resultHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = resultHead;
+        ListNode result = new ListNode(0);
+        ListNode p = l1, q = l2, tmp = result;
         int more = 0;
         while (null != p || null != q) {
-            int x = (null == p) ? 0 : p.val;
-            int y = (null == q) ? 0 : q.val;
-            int sum = x + y + more;
+            int pVal = null == p ? 0 : p.val;
+            int qVal = null == q ? 0 : q.val;
+            int sum = pVal + qVal;
+            tmp.next = new ListNode(sum % 10 + more);
             more = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if (p != null) p = p.next;
-            if (q != null) q = q.next;
+            tmp = tmp.next;
+            if (null != p) p = p.next;
+            if (null != q) q = q.next;
         }
         if (more > 0) {
-            curr.next = new ListNode(more);
+            tmp.next = new ListNode(more);
         }
-        return resultHead.next;
+        return result.next;
     }
 }
