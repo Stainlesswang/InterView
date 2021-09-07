ce102807cb07908ec978f93eab2f1dadde4e3848
allen <allenwangook@gmail.com>
2021-09-07 22:29:16 +0800

    leecode11

--- src/main/java/allen/interview/JavaAlgo/leecode/LC11.java
+++ src/main/java/allen/interview/JavaAlgo/leecode/LC11.java
@@ -5,17 +5,25 @@ package allen.interview.JavaAlgo.leecode;
  * @date 2020/4/21 11:00 PM
  */
 public class LC11 {
+    /**
+     * 盛水最多的容器,左右两个指针向中间寻找,
+     * 停止条件是指针相遇,移动条件是移动边界较小的那个
+     * 理论是木桶效应,左右两个边界,最小的那个此时已经是最优解了
+     * 再也找不到比当前容量更大的比较小这个作为边的容器了
+     *
+     * @param height 边界高度数组
+     * @return 可容纳的最大数量
+     */
     public int maxArea(int[] height) {
-        int l = 0, r = height.length - 1;
         int ans = 0;
-        while (l < r) {
-            int area = Math.min(height[l], height[r]) * (r - l);
-            ans = Math.max(ans, area);
-            if (height[l] <= height[r]) {
-                ++l;
-            }
-            else {
-                --r;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int len = right - left;
+            ans = Math.max(ans, len * (Math.min(height[left], height[right])));
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
         return ans;
