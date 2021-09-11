95e862a38839ce60040f1867d70dac8faef2f15a
allen <allenwangook@gmail.com>
2021-09-11 18:19:16 +0800

    leecode5

--- src/main/java/allen/interview/JavaAlgo/leecode/LC5.java
+++ src/main/java/allen/interview/JavaAlgo/leecode/LC5.java
@@ -6,26 +6,28 @@ package allen.interview.JavaAlgo.leecode;
  */
 public class LC5 {
 
-    public static String longestPalindrome(String s) {
-
-        if (s == null || s.length() < 1) return "";
+    public static String longestPalindrome(String input) {
+        if (null == input || input.length() < 1) return "";
+        //遍历所有可能是最长回文子串的中心,可能是一个字母也可能是两个字母
+        int len = input.length();
+        int ans = 0;
         int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            //以i位置作为中心向左右扩展
-            int len1 = expandAroundCenter(s, i, i);
-            //以i i+1 作为中心 向两边扩展,两边的值不相同停止扩展
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);//得到两个中心最长的字符长度
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        for (int i = 0; i < len; i++) {
+            int oneLen = expend(input, i, i);
+            int twoLen = expend(input, i, i + 1);
+            int currMax = Math.max(oneLen, twoLen);
+            if (currMax > ans) {
+                ans = currMax;
+                start = i - (currMax - 1) / 2;
+                end = i + currMax / 2;
             }
         }
-        return s.substring(start, end + 1);
+        return input.substring(start, end + 1);
     }
 
-    private static int expandAroundCenter(String s, int left, int right) {
-        int L = left, R = right;
+    public static int expend(String s, int start, int end) {
+        int L = start, R = end;
+        //如果 左边的值和右边的值相等,并且左边大于等于0 右边小于等于 len-1  两遍往外扩展
         while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
             L--;
             R++;
@@ -33,10 +35,14 @@ public class LC5 {
         return R - L - 1;
     }
 
-    public static void main(String[] args) {
-        System.out.println(longestPalindrome("habbah"));
-    }
 
+    public static void main(String[] args) {
+
+        String a = "abcdcba";
+        System.out.println(a.length());
+        System.out.println(longestPalindrome(a));
+
+    }
 
 
 }
