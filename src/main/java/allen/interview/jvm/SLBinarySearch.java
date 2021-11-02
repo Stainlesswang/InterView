6185dcd6bcee3f217874176bcc68716f5d8f358c
allen <allenwangook@gmail.com>
2021-11-02 21:06:57 +0800

    leecode34

---
+++ src/main/java/allen/interview/jvm/SLBinarySearch.java
@@ -0,0 +1,40 @@
+package allen.interview.jvm;
+
+import java.util.Arrays;
+
+/**
+ * @author AllenWong
+ * @date 2021/10/31 下午10:01
+ */
+public class SLBinarySearch {
+
+    public static int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1,2};
+
+        int[] posAll = searchRange(nums, 1);
+        System.out.println(Arrays.toString(posAll));
+    }
+}
