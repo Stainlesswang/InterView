3f65619667577cfc26f9e585a4935c30c3bc019b
allen <allenwangook@gmail.com>
2021-12-16 12:15:34 +0800

    update

--- src/main/java/allen/interview/JavaAlgo/leecode/LC48.java
+++ src/main/java/allen/interview/JavaAlgo/leecode/LC48.java
@@ -1,31 +1,42 @@
 package allen.interview.JavaAlgo.leecode;
 
-import java.util.Arrays;
-
 /**
- * @author AllenWong
- * @date 2021/11/21 下午12:04
+ * @author jianqiang8
+ * @description 旋转图像
+ * @date 2021/11/30 10:48 上午
  */
 public class LC48 {
-    public static void main(String[] args) {
-        rotate(new int[][]{{3, 4, 5}, {6, 7, 8}, {9, 10, 11}});
-
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
     }
 
-
-    public static void rotate(int[][] matrix) {
-        String a = "afv";
-        char[] temp = a.toCharArray();
-        Arrays.sort(temp);
-        String orderStr = new String(temp);
-        for (int i = 0; i < matrix.length; i++) {
-            int[] now = matrix[i];
-            for (int j = 0; j < now.length; j++) {
-                System.out.println(now[j]);
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                System.out.println(temp);
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
             }
         }
 
     }
-
-
 }
