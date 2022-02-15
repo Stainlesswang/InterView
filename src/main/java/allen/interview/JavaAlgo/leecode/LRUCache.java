25cb75f151b8801d880b9ae020fe07e8f45a6f81
jianqiang8 <jianqiang8@staff.weibo.com>
2022-02-15 22:21:33 +0800

    LRUCache

--- src/main/java/allen/interview/JavaAlgo/leecode/LRUCache.java
+++ src/main/java/allen/interview/JavaAlgo/leecode/LRUCache.java
@@ -9,32 +9,76 @@ import java.util.Map;
  */
 class LRUCache {
 
-    private final Map<Integer, Integer> dataMap;
+    private final Map<Integer, DataNode> dataMap;
+    private DataNode head;
+    private DataNode tail;
 
-    private int capacity = 8;
+
+    private int size;
+    private int capacity;
 
     public LRUCache(int capacity) {
+        this.size = 0;
         this.capacity = capacity;
         dataMap = new HashMap<>(capacity);
+        head = new DataNode();
+        tail = new DataNode();
+        head.next = tail;
+        tail.prev = head;
     }
 
     public int get(int key) {
-        Integer val;
-        if (null == (val = dataMap.get(key))) {
+        DataNode dataNode = dataMap.get(key);
+        if (null == dataNode) {
             return -1;
         }
-
-        return val;
+        //将该Node 移动到头部
+        move2Head(dataNode);
+        return dataNode.value;
     }
 
     public void put(int key, int value) {
-        synchronized (dataMap) {
-            if (dataMap.keySet().size() < capacity) {
-                dataMap.put(key, value);
+        DataNode dataNode = dataMap.get(key);
+        if (null == dataNode) {
+            DataNode now = new DataNode(key, value);
+            dataMap.put(key, now);
+            add2Head(now);
+            ++size;
+            if (size > capacity) {
+                DataNode tail = removeTail();
+                dataMap.remove(tail.key);
+                --size;
             }
+        } else {
+            dataNode.value = value;
+            move2Head(dataNode);
 
         }
 
+    }
+
+    private void add2Head(DataNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DataNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void move2Head(DataNode node) {
+        removeNode(node);
+        add2Head(node);
+
+    }
+
+    private DataNode removeTail() {
+        DataNode tailDataNode = tail.prev;
+        removeNode(tailDataNode);
+        return tailDataNode;
     }
 
     private static class DataNode {
